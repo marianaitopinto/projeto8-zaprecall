19e6e3077b108ea3a34acadb8e8ef15b16a8c7fb
Mariana Ito Pinto <marianaitopinto@gmail.com>
2022-03-21 00:31:34 -0300

    adding infos to answered questions

--- src/Render.js
+++ src/Render.js
@@ -1,6 +1,7 @@
 import React from "react";
 export default function Render({ questionNo, question, answer }) {
     const [step, setStep] = React.useState("");
+    const colors = ["red", "yellow", "green"];
 
     if (step === "") {
         return (
@@ -13,24 +14,39 @@ export default function Render({ questionNo, question, answer }) {
 
     else if (step === "question") {
         return (
-            <div className="deckBox biggerBox" onClick={() => setStep("answer")}>
+            <div className="deckBox biggerBox">
                 <p>{question}</p>
-                <img className="setinha" src="img/setinha.png" />
+                <img className="setinha" src="img/setinha.png" onClick={() => setStep("answer")} />
             </div>
         )
     }
 
     else if (step === "answer") {
+        
         return (
             <div className="deckBox answerBox biggerBox">
                 <p className="answer">{answer}</p>
                 <div>
-                    <button className="answerButton redButton">Não lembrei</button>
-                    <button className="answerButton yellowButton">Quase não lembrei</button>
-                    <button className="answerButton greenButton">Zap!</button>
+                    <button className="answerButton redButton" onClick={() => setStep("red")}>Não lembrei</button>
+                    <button className="answerButton yellowButton" onClick={() => setStep("yellow")}>Quase não lembrei</button>
+                    <button className="answerButton greenButton" onClick={() => setStep("green")}>Zap!</button>
                 </div>
             </div>
         )
     }
 
+    else if ((step === "red") || (step === "yellow") || (step === "green")) {
+        return (
+        <Finished step={step} questionNo={questionNo} />
+        )
+    }
+}
+
+function Finished({ step, questionNo }) {
+    return (
+        <div className={`deckBox ${step}`}>
+            <p>{questionNo}</p>
+            <img className="answerIcon" src={`img/${step}.png`} />
+        </div>
+    )
 }
