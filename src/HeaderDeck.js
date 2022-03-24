ecccc3db6d50cc9b3a4076cd26be68a87ec8a0f6
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2022-03-24 09:53:35 -0300

    Preparando revisão de código

--- src/HeaderDeck.js
+++
@@ -1,8 +0,0 @@
-export default function HeaderDeck() {
-    return (
-        <div className="header">
-            <img className="logo2" src="img/logo-pequeno.png" alt="logo"/>
-            <p className="title2">ZapRecall</p>
-        </div>
-    )
-}
